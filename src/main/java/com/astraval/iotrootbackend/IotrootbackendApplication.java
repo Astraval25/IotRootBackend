7920cdd69b5288bf2c9b2212c50567b7beded1a5
Astraval <admin@astraval.com>
2026-02-24 19:43:38 +0530

    project init

---
+++ src/main/java/com/astraval/iotrootbackend/IotrootbackendApplication.java
@@ -0,0 +1,13 @@
+package com.astraval.iotrootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotrootbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotrootbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astraval/iotrootbackend/IotrootbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astraval.iotrootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotrootbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
